d877c812b06227bc2395c837edf244ac63eb6ccd
Kurchavov Daniil <daniil.kurchavov@gmail.com>
2025-04-11 18:59:17 +0300

    commit6

--- src/blocks/AppContest.vue
+++ src/blocks/AppContest.vue
@@ -2,19 +2,29 @@
 import AppButton from '@/components/AppButton.vue';
 import AppTag from '@/components/AppTag.vue';
 import AppLargeTitle from '@/components/AppLargeTitle.vue';
-import AppRegularText from '@/components/AppRegularText.vue'
+import AppRegularText from '@/components/AppRegularText.vue';
 </script>
 
 <template>
-  <AppTag>Технологический конкурс</AppTag>
-  <AppLargeTitle />
-  <AppRegularText>Предложите свое уникальное решение<br>в сфере технологических разработок</AppRegularText>
-  <AppButton>Принять участие</AppButton>
-  <AppRegularText class="text__opacity">Заявки принимаются до 27 марта</AppRegularText>
+  <div class="app-contest">
+    <AppTag class="app-contest__tag" data-tete="aaaa">Технологический конкурс</AppTag>
+    <AppLargeTitle />
+    <AppRegularText
+      >Предложите свое уникальное решение<br />в сфере технологических разработок</AppRegularText
+    >
+    <AppButton>Принять участие</AppButton>
+    <AppRegularText class="text__opacity">Заявки принимаются до 27 марта</AppRegularText>
+  </div>
 </template>
 
 <style scoped lang="scss">
-    .text__opacity {
-        opacity: 0.4;
-    }
+.text__opacity {
+  opacity: 0.4;
+}
+
+.app-contest {
+  &__tag {
+    margin-top: 12px;
+  }
+}
 </style>
