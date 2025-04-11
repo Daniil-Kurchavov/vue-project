9423330db70da1617c9e58ead84a4e9ddefa3906
Kurchavov Daniil <daniil.kurchavov@gmail.com>
2025-04-11 16:33:53 +0300

    second

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <button class="button">Принять участие</button>
+</template>
+
+<style scoped>
+.button {
+  padding: 12px 24px;
+  background: linear-gradient(to top left, #14d461, #12acdd);
+  border-radius: 12px;
+}
+</style>
